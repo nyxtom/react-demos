7a78461f8748e3977b826d9dc365abf69734224f
Thomas Holloway <nyxtom@gmail.com>
2021-07-12 09:23:10 -0500

    Add topic route with use of params, location, path

--- 3-router/README.md
+++ 3-router/README.md
@@ -38,6 +38,20 @@ const Dashboard = () => (
   <div>Dashboard</div>
 )
 
+const Topic = () => {
+  const params = useParams()
+  const match = useRouteMatch()
+  const location = useLocation()
+  const { path, url } = match
+  console.log({ params, match, path, location })
+
+  return (
+    <div>
+      <h3>Topic: {params.topicId}</h3>
+    </div>
+  )
+}
+
 const App = () => (
   <Router>
     <div id="app" className="center">
@@ -46,12 +60,14 @@ const App = () => (
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/topics/1234">Topic 1234</Link></li>
         </ul>
       </div>
       <div className="container">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/dashboard" component={Dashboard} />
+          <Route path="/topics/:topicId" component={Topic} />
         </Switch>
       </div>
     </div>
--- 3-router/src/index.js
+++ 3-router/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { useLocation, useParams, useRouteMatch, BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import './App.css'
 
@@ -12,6 +12,20 @@ const Dashboard = () => (
   <div>Dashboard</div>
 )
 
+const Topic = () => {
+  const params = useParams()
+  const match = useRouteMatch()
+  const location = useLocation()
+  const { path, url } = match
+  console.log({ params, match, path, location })
+
+  return (
+    <div>
+      <h3>Topic: {params.topicId}</h3>
+    </div>
+  )
+}
+
 const App = () => (
   <Router>
     <div id="app" className="center">
@@ -20,12 +34,14 @@ const App = () => (
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/topics/1234">Topic 1234</Link></li>
         </ul>
       </div>
       <div className="container">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/dashboard" component={Dashboard} />
+          <Route path="/topics/:topicId" component={Topic} />
         </Switch>
       </div>
     </div>

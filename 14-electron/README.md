b6a9f0691a61866c65b6c8f16cc694ecada9f434
Thomas Holloway <nyxtom@gmail.com>
2021-08-01 10:43:27 -0500

    Setup simple electron + vite + react demo

---
+++ 14-electron/README.md
@@ -0,0 +1,9 @@
+# electron
+
+Simple react application demonstrating how to build an electron desktop app with React
+
+## Setup dependencies
+
+```javascript
+
+```
---
+++ 14-electron/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslint from '@rollup/plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    {
+      ...eslint({
+        include: ['**/**.jsx', '**/**.js'],
+        exclude: ['node_modules/**', 'dist/**'],
+        configFile: '.eslintrc.json',
+        throwOnError: true,
+        throwOnWarning: true,
+      }),
+      enforce: 'pre',
+    },
+    reactRefresh(),
+  ],
+})

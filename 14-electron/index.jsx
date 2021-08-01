11f960c1ac89b11d6147d5613c1330a9eb9a3e28
Thomas Holloway <nyxtom@gmail.com>
2021-08-01 13:31:21 -0500

    Update styles, layout for electron app, add scripts

--- 14-electron/index.jsx
+++ 14-electron/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './index.css'
+
 const App = () => (
   <div id="app">
-    <h1>Hello world</h1>
+    <header>
+      <h1>Hello world</h1>
+    </header>
+    <div className="container">
+      <section className="sidebar">
+        Sidebar
+      </section>
+      <section className="content">
+        Messages
+      </section>
+    </div>
   </div>
 )
 
